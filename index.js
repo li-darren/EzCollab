2dc2d315e464bcdd8266cf69f4da62a8bdd6e36b
li-darren <d338li@edu.uwaterloo.ca>
2020-04-14 20:01:22 -0400

    change index back

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 
 var socket = require('socket.io');
-const server = require('http').Server(app);
 
 //setup the app
 
 var app = express ();
 
-server.listen(5000, function (){
+var server = app.listen(5000, function (){
     console.log("Listening to requests on port 5000");
 });
 
